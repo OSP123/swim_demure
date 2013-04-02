c2951be3a2d806f31adae79f02deb90ce51073de
OSP123 <omar@getsmartify.com>
2013-04-01 22:30:43 -0700

    added swim_demure to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwimDemureRails::Application.config.secret_token = '2e423266ccabce36ac66da6e5f3e4117d9d068823baad19dc5c9726b1de1c4d99e617c71bf81d1d13f2f6724ae612f1d3ed4abc755bdfec07cc2744b2a85dbad'
